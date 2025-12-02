70d649ba2a355eeba912c11f863080f443eb01c5
matisito23 <matiasmorales811@gmail.com>
2025-12-02 20:21:36 -0300

    Add WhiteNoise middleware to WSGI application
    Integrate WhiteNoise middleware for static file handling.

--- drfsimplecrud/wsgi.py
+++ drfsimplecrud/wsgi.py
@@ -1,6 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise.middleware import WhiteNoise 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfsimplecrud.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application) 
